93f88be4109f701c802e4449db1f67e472b1dece
dltslt <87982398+dltslt@users.noreply.github.com>
2026-06-06 22:12:45 +0200

    feat: multiple images in one window

--- sources/test_image.c
+++ sources/test_image.c
@@ -1,4 +1,5 @@
 #include "Retro_MFA.h"
+#include "math.h"
 
 static void prt_32byte(t_byte* buf, ssize_t adress)
 {
@@ -54,26 +55,38 @@ void single_sprite_test(t_data* data, ssize_t adress)
 {
 	t_sprite sprite;
 	t_mlxu_2d px = {0,0,0};
+	static t_mlxu_2d off; // to put them next to each other
 	uint16_t col_encoded;
 	ssize_t frame;
-	int color, xoff = 0;
+	int color;
+	static int ymax;
 	get_asset_data(data, adress, &sprite);
 	frame = sprite.width * sprite.height * 2; // 2 is only for here
 	adress += 32;
+	// put everything next to each other
+	if (ymax < sprite.height - 1)
+	 ymax = sprite.height;
+	if (off.x + sprite.width >= data->visual.active.win->size.x)
+	{
+		off.x = 0;
+		off.y += ymax;
+		if (off.y + sprite.height >= data->visual.active.win->size.y)
+			exit(1); // FIXME
+	}
+	// ONE image
 	for (ssize_t i = 0; i < frame; i += 2)
 	{
 		color = 0;
 		col_encoded = 0;
 		col_encoded = build_2_bytes_int(&data->file_buf[adress + i]);
 		color = col_encoded | data->file_buf[adress + i + frame] << 24;
-		mlxu_pixel_put_buffer(&data->visual, px.x + xoff, px.y, color);
+		mlxu_pixel_put_buffer(&data->visual, px.x + off.x, px.y + off.y, color);
 		advance_px(&px, sprite.width - 1);
 		if (px.y == sprite.height - 1 && px.x == sprite.width - 1)
 		{
 			px.x = 0;
 			px.y = 0;
-			xoff = sprite.width;
+			off.x += sprite.width;
 		}
 	}
-	rnd_frame(data);
 }
